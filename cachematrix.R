b9a82cae88571f0587f69b2a5c43c9d95facc488
bleneak <bleneak@gmail.com>
2017-09-22 23:00:01 +0300

    Submit work for my Coursera R-Programming class, programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions provide a mechanism for under-the-hood caching of the result
+## of the potentially expensive inverse operation for a given matrix.
 
-## Write a short comment describing this function
-
+## This is the function to create an object for a matrix. The result will be
+## used to get the inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
+    #initialize the inverse to NULL. Better to be lazy while possible.
+    inv<-NULL;
 
+    #subfunction is used to escape re-initialization of the inverse.
+    getInv<-function(){
+        
+        #if the inverse has not yet been calculated, gotta do it now.
+        if(is.null(inv)){
+            inv<<-solve(x)
+        }
+        
+        #return the inverse.
+        inv
+    }
+
+    #return the subfunction as an object.
+    getInv
 }
 
 
-## Write a short comment describing this function
+## Use an object returned by makeCacheMatrix, and return the inverse of the
+## matrix that was passed to it to create the object.
+cacheSolve <- function(x) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #the object is in fact a function, which returns the inverse when called. 
+    x()
 }
